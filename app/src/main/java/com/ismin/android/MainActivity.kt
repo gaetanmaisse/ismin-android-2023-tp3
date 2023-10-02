4bbfb19654685e8ec2f5f54feac9358975ccde2a
Gaëtan Maisse <gaetanmaisse@gmail.com>
2023-10-02 18:21:11 +0200

    Copy src from previous TP

--- app/src/main/java/com/ismin/android/MainActivity.kt
+++ app/src/main/java/com/ismin/android/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.ismin.android
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val book = it.data?.getSerializableExtra(CREATED_BOOK) as Book
+                Toast.makeText(this, "Book to create:$book", Toast.LENGTH_LONG).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.a_main_btn_create_book).setOnClickListener {
+            val intent = Intent(this, CreateBookActivity::class.java)
+            startForResult.launch(intent)
+        }
     }
 }
